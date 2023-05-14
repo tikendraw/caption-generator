3422c832aac6442fb7f5b437c89c0a77f662e945
tikendraw <tikendraksahu1029@gmail.com.com>
2023-05-14 11:39:52 +0530

    get_data

--- get_data.py
+++ get_data.py
@@ -5,11 +5,12 @@ from funcyou.dataset import download_kaggle_dataset
 from pathlib import Path
 from config import config
 from preprocessing import clean_df
+from zipfile import ZipFile
 
 dataset_url = Path('https://www.kaggle.com/datasets/hsankesara/flickr-image-dataset')
 api_command = "kaggle datasets download -d hsankesara/flickr-image-dataset"
 
-dest_caption_file_dir = config.caption_file
+dest_caption_file_dir = config.raw_caption_file
 src_caption_file_dir = Path("./flickr30k_images/flickr30k_images/results.csv")
 
 def move_to_dest():
@@ -30,18 +31,18 @@ def move_to_dest():
 
 if __name__=='__main__':
 
-    # print("Downloading...")
-    # download_kaggle_dataset(api_command, kaggle_json_filepath='./kaggle.json')
-    # print("Downloaded.")
+    print("Downloading...")
+    download_kaggle_dataset(api_command, kaggle_json_filepath='./kaggle.json')
+    print("Downloaded.")
     
 
-    # print('Extracting...')
-    # with ZipFile('flickr-image-dataset.zip') as zip:
-    #     zip.extractall()
+    print('Extracting...')
+    with ZipFile('flickr-image-dataset.zip') as zip:
+        zip.extractall()
 
-    # print('Moving...')
-    # move_to_dest()
-    # print('Done')
+    print('Moving...')
+    move_to_dest()
+    print('Done')
 
     clean_df()
     print('Cleaned.')
