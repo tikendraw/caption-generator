7cbbdb9f35ed810b8bdab3d1d6ceafe113e3565c
tikendraw <tikendraksahu1029@gmail.com.com>
2023-05-27 23:40:11 +0530

    new transformer

--- transformer.py
+++ transformer.py
@@ -72,336 +72,230 @@ data_augmentation = keras.Sequential(
 )
 
 
-def positional_encoding(length, depth):
-    depth = depth/2
+# def positional_encoding(length, depth):
+#     depth = depth/2
 
-    positions = np.arange(length)[:, np.newaxis]     # (seq, 1)
-    depths = np.arange(depth)[np.newaxis, :]/depth   # (1, depth)
+#     positions = np.arange(length)[:, np.newaxis]     # (seq, 1)
+#     depths = np.arange(depth)[np.newaxis, :]/depth   # (1, depth)
 
-    angle_rates = 1 / (10000**depths)         # (1, depth)
-    angle_rads = positions * angle_rates      # (pos, depth)
+#     angle_rates = 1 / (10000**depths)         # (1, depth)
+#     angle_rads = positions * angle_rates      # (pos, depth)
 
-    pos_encoding = np.concatenate(
-        [np.sin(angle_rads), np.cos(angle_rads)],
-        axis=-1)        
+#     pos_encoding = np.concatenate(
+#         [np.sin(angle_rads), np.cos(angle_rads)],
+#         axis=-1)        
 
-    return tf.cast(pos_encoding, dtype=tf.float32)
-
-
-# Positional embedding For Image
-class PositionalEmbedding(tf.keras.layers.Layer):
-  def __init__(self, vocab_size, d_model):
-    super().__init__()
-    self.d_model = d_model
-    self.embedding = tf.keras.layers.Embedding(vocab_size, d_model, mask_zero=True) 
-    self.pos_encoding = positional_encoding(length=2048, depth=d_model)
-
-  def compute_mask(self, *args, **kwargs):
-    return self.embedding.compute_mask(*args, **kwargs)
-
-  def call(self, x):
-    length = tf.shape(x)[1]
-    x = self.embedding(x)
-    # This factor sets the relative scale of the embedding and positonal_encoding.
-    x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-    x = x + self.pos_encoding[tf.newaxis, :length, :]
-    return x
+#     return tf.cast(pos_encoding, dtype=tf.float32)
 
 
-class Patches(tf.keras.layers.Layer):
-    def __init__(self, patch_size):
-        super().__init__()
-        self.patch_size = patch_size
+# # Positional embedding For Image
+# class PositionalEmbedding(tf.keras.layers.Layer):
+#   def __init__(self, vocab_size, d_model):
+#     super().__init__()
+#     self.d_model = d_model
+#     self.embedding = tf.keras.layers.Embedding(vocab_size, d_model, mask_zero=True) 
+#     self.pos_encoding = positional_encoding(length=2048, depth=d_model)
 
-    def call(self, images):
-        batch_size = tf.shape(images)[0]
-        patches = tf.image.extract_patches(
-            images=images,
-            sizes=[1, self.patch_size, self.patch_size, 1],
-            strides=[1, self.patch_size, self.patch_size, 1],
-            rates=[1, 1, 1, 1],
-            padding="VALID",
-        )
-        patch_dims = patches.shape[-1]
-        # (patches.shape)
-        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
-        return patches
+#   def compute_mask(self, *args, **kwargs):
+#     return self.embedding.compute_mask(*args, **kwargs)
+
+#   def call(self, x):
+#     length = tf.shape(x)[1]
+#     x = self.embedding(x)
+#     # This factor sets the relative scale of the embedding and positonal_encoding.
+#     x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+#     x = x + self.pos_encoding[tf.newaxis, :length, :]
+#     return x
 
 
-class PatchEncoder(tf.keras.layers.Layer):
-    def __init__(self, num_patches, d_model):
-        super().__init__()
-        self.num_patches = num_patches
-        self.projection = Dense(units=d_model)
-        self.position_embedding = Embedding(
-            input_dim=num_patches, output_dim=d_model
-        )
+# class Patches(tf.keras.layers.Layer):
+#     def __init__(self, patch_size):
+#         super().__init__()
+#         self.patch_size = patch_size
 
-    def call(self, patch):
+#     def call(self, images):
+#         batch_size = tf.shape(images)[0]
+#         patches = tf.image.extract_patches(
+#             images=images,
+#             sizes=[1, self.patch_size, self.patch_size, 1],
+#             strides=[1, self.patch_size, self.patch_size, 1],
+#             rates=[1, 1, 1, 1],
+#             padding="VALID",
+#         )
+#         patch_dims = patches.shape[-1]
+#         # (patches.shape)
+#         patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+#         return patches
+
+
+# class PatchEncoder(tf.keras.layers.Layer):
+#     def __init__(self, num_patches, d_model):
+#         super().__init__()
+#         self.num_patches = num_patches
+#         self.projection = Dense(units=d_model)
+#         self.position_embedding = Embedding(
+#             input_dim=num_patches, output_dim=d_model
+#         )
+
+#     def call(self, patch):
         
-        positions = tf.range(start=0, limit=self.num_patches, delta=1)
-#         tf.print(positions.shape)
-        return self.projection(patch) + self.position_embedding(positions)
+#         positions = tf.range(start=0, limit=self.num_patches, delta=1)
+# #         tf.print(positions.shape)
+#         return self.projection(patch) + self.position_embedding(positions)
+
+# Embedding
+
+class SeqEmbedding(tf.keras.layers.Layer):
+    def __init__(self, vocab_size, max_length, d_model):
+        super().__init__()
+        self.pos_embedding = tf.keras.layers.Embedding(input_dim=max_length, output_dim=d_model)
+
+        self.token_embedding = tf.keras.layers.Embedding(
+            input_dim=vocab_size,
+            output_dim=d_model,
+            mask_zero=True)
+
+        self.add = tf.keras.layers.Add()
+
+    def call(self, seq):
+        seq = self.token_embedding(seq) # (batch, seq, d_model)
+
+        x = tf.range(tf.shape(seq)[1])  # (seq)
+        x = x[tf.newaxis, :]  # (1, seq)
+        x = self.pos_embedding(x)  # (1, seq, d_model)
+
+        return self.add([seq,x])
+
 
 # Attention
-class BaseAttention(tf.keras.layers.Layer):
-    def __init__(self, **kwargs):
+
+class CrossAttention(tf.keras.layers.Layer):
+    def __init__(self,**kwargs):
         super().__init__()
         self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        self.add = tf.keras.layers.Add() 
         self.layernorm = tf.keras.layers.LayerNormalization()
-        self.add = tf.keras.layers.Add()
 
+    def call(self, x, context, **kwargs):
+        attn, attention_scores = self.mha(
+                    query=x, value=context,
+                    return_attention_scores=True)
 
-class CrossAttention(BaseAttention):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.last_attn_scores=None
-    
-    def call(self, x, context):
-        attn_output, attn_scores = self.mha(
-            query=x,
-            key=context,
-            value=context,
-            return_attention_scores=True)
+        self.last_attention_scores = attention_scores
 
-        # Cache the attention scores for plotting later.
-        self.last_attn_scores = attn_scores
-
-        x = self.add([x, attn_output])
-        x = self.layernorm(x)
-
-        return x
-
-
-class GlobalSelfAttention(BaseAttention):
-  def call(self, x):
-    attn_output = self.mha(
-        query=x,
-        value=x,
-        key=x)
-    # tf.print('attn_output: ',attn_output.shape)
-    x = self.add([x, attn_output])
-    # tf.print('concat: ',x.shape)
-
-    x = self.layernorm(x)
-    # tf.print('layernorm: ',x.shape)
-
-    return x
-
-class CausalSelfAttention(BaseAttention):
-  def call(self, x):
-    attn_output = self.mha(
-        query=x,
-        value=x,
-        key=x,
-        use_causal_mask = True)
-    x = self.add([x, attn_output])
-    x = self.layernorm(x)
-    return x
-
-
-class FeedForword(tf.keras.layers.Layer):
-    def __init__(self, d_model, dff, dropout_rate = 0.1):
-        super().__init__()
-
-        
-        self.seq = tf.keras.Sequential([
-            Dense(dff, activation = 'relu'),
-            Dense(d_model),
-            Dropout(dropout_rate)
-        ])
-        self.add = tf.keras.layers.Add()
-        self.layernorm = tf.keras.layers.LayerNormalization()
-        
-    def call(self, x):
-        x = self.add([x, self.seq(x)])
+        x = self.add([x, attn])
         return self.layernorm(x)
 
 
-class EncoderLayer(tf.keras.layers.Layer):
-    def __init__(self, *, d_model, num_heads, dff, dropout_rate=0.1):
+
+class CausalSelfAttention(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
         super().__init__()
-        
-        self.self_attention = GlobalSelfAttention(
-            key_dim=d_model,
-            num_heads=num_heads,
-            dropout=dropout_rate
-        )
-        
-        self.ffn = FeedForword(d_model=d_model, dff=dff,dropout_rate=dropout_rate)
-        
+        self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+        # Use Add instead of + so the keras mask propagates through.
+        self.add = tf.keras.layers.Add() 
+        self.layernorm = tf.keras.layers.LayerNormalization()
 
     def call(self, x):
-        x = self.self_attention(x)
-        x = self.ffn(x)
-        return x
-        
+        attn = self.mha(query=x, value=x,
+                        use_causal_mask=True)
+        x = self.add([x, attn])
+        return self.layernorm(x)
 
 
-class Encoder(tf.keras.layers.Layer):
-    def __init__(self, num_layers, d_model, num_heads, dff, patch_size, num_patches, dropout_rate=0.1):
+class FeedForward(tf.keras.layers.Layer):
+    def __init__(self, dff, d_model, dropout_rate=0.1):
         super().__init__()
+        self.seq = tf.keras.Sequential([
+            tf.keras.layers.Dense(units=dff, activation='relu'),
+            tf.keras.layers.Dense(units=d_model),
+            tf.keras.layers.Dropout(rate=dropout_rate),
+        ])
 
-        self.d_model = d_model
-        self.num_layers = num_layers
-        self.num_heads = num_heads
-        self.dff = dff
-        self.patch_size = patch_size
-        self.num_patches = num_patches
-        self.dropout_rate = dropout_rate
-        self.augment = data_augmentation
-        
-        self.patches = Patches(patch_size)
-        # Encode patches.
-        self.encoded_patches = PatchEncoder(num_patches, d_model)
-        
-
-        self.enc_layers = [
-            EncoderLayer(d_model=d_model,
-                        num_heads=num_heads,
-                        dff=dff,
-                        dropout_rate=dropout_rate)
-            for _ in range(num_layers)]
-        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+        self.layernorm = tf.keras.layers.LayerNormalization()
 
     def call(self, x):
-        # `x` is token-IDs shape: (batch, images)
-        x = self.augment(x)
-        x = self.patches(x)  # Shape `(batch_size, num_patches, -1)`.
-        x = self.encoded_patches(x) # Shape `(batch_size, num_patches, d_model)`.
-        # Add dropout.
-        x = self.dropout(x)
+        x = x + self.seq(x)
+        return self.layernorm(x)
 
-        for i in range(self.num_layers):
-            x = self.enc_layers[i](x)
-
-        return x  # Shape `(batch_size, num_patches, d_model)`.
 
 
 class DecoderLayer(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads, dff, dropout_rate=0.1):
-        super(DecoderLayer, self).__init__()
-
-        self.causal_attention = CausalSelfAttention(
-            num_heads=num_heads,
-            key_dim = d_model, 
-            dropout= dropout_rate
-            )
-        
-        self.cross_attention = CrossAttention(
-            num_heads=num_heads,
-            key_dim = d_model, 
-            dropout= dropout_rate
-            )
-        
-        self.ffn = FeedForword(d_model=d_model, dff=dff,dropout_rate=dropout_rate)
-               
-        self.last_attn_scores = self.cross_attention.last_attn_scores
-
-       
-    def call(self, x, context):
-        x = self.causal_attention(x)
-        x = self.cross_attention(x=x, context = context)
-        x = self.ffn(x)
-        return x
-        
-
-class Decoder(tf.keras.layers.Layer):
-    def __init__(self, num_layers, d_model, num_heads, dff, vocab_size, dropout_rate=0.1):
-        super().__init__()
-        
-        self.num_layers = num_layers 
-        self.d_model = d_model
-        self.num_heads = num_heads 
-        self.dff = dff
-        self.vocab_size = vocab_size
-        self.dropout_rate = dropout_rate=0.1
-        
-        self.positional_embedding = PositionalEmbedding(vocab_size=vocab_size, d_model=d_model)
-        
-        self.dec_layers = [
-            DecoderLayer(d_model=d_model, num_heads=num_heads,
-                        dff=dff, dropout_rate=dropout_rate)
-            for _ in range(num_layers)]
-
-        
-        self.dropout = tf.keras.layers.Dropout(dropout_rate)
-        self.last_attn_scores = None
-
-
-    def call(self, x, context):
-        # tf.print('x: ', x.shape)
-        # tf.print('context: ', context.shape)
-        
-        x = self.positional_embedding(x)
-        # tf.print('pos-emb x: ', x.shape)
-
-        for i in range(self.num_layers):
-            x = self.dec_layers[i](x=x, context=context)
-        
-            
-        self.last_attn_scores = self.dec_layers[-1].last_attn_scores
-        # tf.print('afte tra x : ', x.shape)
-
-        return x
-
-
-class CaptionGenerator(tf.keras.Model):
-    def __init__(self, num_layers, d_model, num_heads, dff, vocab_size, patch_size, num_patches, dropout_rate=0.1):
+    def __init__(self, dff, d_model, num_heads=1, dropout_rate=0.1):
         super().__init__()
 
-        self.encoder = Encoder(
-                            num_layers=num_layers,
-                            d_model=d_model,
-                            num_heads=num_heads,
-                            dff=dff,
-                            patch_size=patch_size,
-                            num_patches=num_patches,
-                            dropout_rate=dropout_rate,
-                            )
-        
-        self.decoder = Decoder(
-                            num_layers=num_layers,
-                            d_model=d_model,
-                            num_heads=num_heads,
-                            dff=dff,
-                            vocab_size=vocab_size,
-                            dropout_rate=dropout_rate,
-                            )
-        
-        self.final_layer = tf.keras.layers.Dense(vocab_size
-                               
-        )        
-        self.decoder = Decoder(
-                            num_heads=num_heads,
-                            num_layers=num_layers,
-                            d_model=d_model,
-                            dff=dff,
-                            vocab_size=vocab_size,
-                            dropout_rate=dropout_rate,
-                            )
-        
-        self.final_layer = tf.keras.layers.Dense(vocab_size)
-        
-    def call(self, inputs):  # sourcery skip: inline-immediately-returned-variable, use-contextlib-suppress
-        img, txt  = inputs
+        self.self_attention = CausalSelfAttention(num_heads=num_heads,
+                                                    key_dim=d_model,
+                                                    dropout=dropout_rate)
+        self.cross_attention = CrossAttention(num_heads=num_heads,
+                                                key_dim=d_model,
+                                                dropout=dropout_rate)
+        self.ff = FeedForward(dff=dff, d_model=d_model, dropout_rate=dropout_rate)
 
-        img = self.encoder(img)  # (batch_size, num_patches, d_model)
 
-        x = self.decoder(x=txt, context=img)  # (batch_size, target_len, d_model)
+    def call(self, inputs, training=False):
+        img_in, txt_in = inputs
 
-        # Final linear layer output.
-        logits = self.final_layer(x)  # (batch_size, max_len, target_vocab_size)
-        
-        try:
-      # Drop the keras mask, so it doesn't scale the losses/metrics.
-      # b/250038731
-            del logits._keras_mask
-        except AttributeError:
-            pass
+        # Text input
+        txt_in = self.self_attention(x=txt_in)
 
-        # Return the final output and the attention weights.
-        return logits
+        txt_in = self.cross_attention(x = txt_in, context=img_in)
+
+        self.last_attention_scores = self.cross_attention.last_attention_scores
+
+        txt_in = self.ff(txt_in)
+
+        return txt_in        
+
+
+class TokenOutput(tf.keras.layers.Layer):
+    def __init__(self, tokenizer, banned_tokens=('', '[UNK]', '[START]'), **kwargs):
+        super().__init__()
+
+        self.dense = tf.keras.layers.Dense(
+            units=tokenizer.vocabulary_size(), **kwargs)
+        self.tokenizer = tokenizer
+        self.banned_tokens = banned_tokens
+
+        self.bias = None
+
+    def adapt(self, ds):
+        counts = collections.Counter()
+        vocab_dict = {name: id 
+                        for id, name in enumerate(self.tokenizer.get_vocabulary())}
+
+        for tokens in tqdm.tqdm(ds):
+            counts.update(tokens.numpy().flatten())
+
+        counts_arr = np.zeros(shape=(self.tokenizer.vocabulary_size(),))
+        counts_arr[np.array(list(counts.keys()), dtype=np.int32)] = list(counts.values())
+
+        counts_arr = counts_arr[:]
+        for token in self.banned_tokens:
+            counts_arr[vocab_dict[token]] = 0
+
+        total = counts_arr.sum()
+        p = counts_arr/total
+        p[counts_arr==0] = 1.0
+        log_p = np.log(p)  # log(1) == 0
+
+        entropy = -(log_p*p).sum()
+
+        print()
+        print(f"Uniform entropy: {np.log(self.tokenizer.vocabulary_size()):0.2f}")
+        print(f"Marginal entropy: {entropy:0.2f}")
+
+        self.bias = log_p
+        self.bias[counts_arr==0] = -1e9
+
+    def call(self, x):
+        x = self.dense(x)
+        # TODO(b/250038731): Fix this.
+        # An Add layer doesn't work because of the different shapes.
+        # This clears the mask, that's okay because it prevents keras from rescaling
+        # the losses.
+        return x + self.bias
+
 
 
 if __name__=='__main__':
